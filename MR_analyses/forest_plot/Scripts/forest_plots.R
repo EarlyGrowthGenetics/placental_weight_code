07b6fdbb500a09d900a354d3a45cac2458057f7d
mvaudel <mvaudel@gmail.com>
2022-11-10 14:01:54 +0100

    Tried integrating the legend to the plot for the MR figure.

--- MR_analyses/forest_plot/Scripts/forest_plots.R
+++ MR_analyses/forest_plot/Scripts/forest_plots.R
@@ -2,8 +2,7 @@ library(here)
 library(glue)
 library(dplyr)
 library(ggplot2)
-library(patchwork)
-library(ggpubr)
+library(grid)
 
 df <- read.table(
   file = file.path(here(), 'MR_analyses/forest_plot/Data/plots_data.csv.gz'),
@@ -29,6 +28,10 @@ df <- df %>% arrange(exposure, Genome, Analysis)
 
 df$person <- factor(df$person, levels = rev(unique(df$person)))
 
+df$y <- as.numeric(df$person)
+df$y <- ifelse(df$y == min(df$y), df$y + 0.5, df$y)
+df$y <- ifelse(df$y == max(df$y), df$y - 0.5, df$y)
+
 #### Convert BP to 10mm Hg instead of 1 mm Hg
 
 df$Beta <- ifelse((df$exposure=="SBP" | df$exposure== "DBP"), df$Beta*10, df$Beta)
@@ -39,7 +42,80 @@ df$Se <- ifelse((df$exposure=="SBP" | df$exposure== "DBP"), df$Se*10, df$Se)
 df$Analysis[df$Analysis=="Meta"] <- "Meta-analysis"
 df$Analysis[df$Analysis=="WLM"] <- "WLM-adjusted"
 
-###### Plot data
+
+###### Plot
+
+forest_plot <- ggplot() + 
+  geom_vline(
+    xintercept = 0
+  ) +
+  geom_segment(
+    data = df,
+    mapping = aes(
+      x = Beta - qnorm(0.975) * Se,
+      xend = Beta + qnorm(0.975) * Se,
+      y = y,
+      yend = y,
+      color = Genome
+    ),
+    size = 0.8
+  ) +
+  geom_point(
+    data = df,
+    mapping = aes(
+      x = Beta,
+      y = y,
+      color = Genome,
+      shape = Analysis
+    ),
+    size = 3
+  ) + 
+  facet_grid(
+    exposure ~ .,
+    scales="free",
+    space="free",
+    labeller = label_wrap_gen(
+      width = 2,
+      multi_line = T
+      )
+    ) +
+  scale_x_continuous(
+    name = "Effect Size [95% CI]"
+  ) +
+  scale_y_continuous(
+    breaks = c(1.75, 3.25),
+    labels = c("Fetal", "Maternal"),
+    limits = c(1, 4)
+  ) +
+  scale_color_manual(
+    values = c("#009E73","#D55E00"),
+    guide = "none"
+    ) + 
+  theme_bw(
+      base_size = 24
+    ) + 
+  scale_shape(
+    guide = guide_legend(
+      override.aes = list(size = 5)
+    )
+  ) +
+  theme(
+    panel.background = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    axis.title.y = element_blank(),
+    legend.position = c(0.13, 0.96),
+    legend.title = element_blank(),
+    legend.background = element_rect(
+      fill = "white",
+      color = "grey20",
+      size = 0.5
+    ),
+    legend.text = element_text(size = 18),
+    strip.text = element_text(size = 18),
+    legend.margin = margin(t = -3, r = 2, b = 2, l = 2, unit = "mm")
+  ) 
 
 
 p1 <- ggplot(df, aes(Beta, person)) 
@@ -70,9 +146,9 @@ p1 <- p1 + xlab("") + ylab("") + theme_bw() +
 
 # save the plot
 
-figure_path <- file.path(here(), 'MR_analyses/forest_plot/Fig6_Mendelian_Randomization.png')
+figure_path <- file.path(here(), 'MR_analyses/forest_plot/Fig6_Mendelian_Randomization_2.png')
 png(figure_path,
-    width=1300, height=700)
-p1
+    width=900, height=900)
+grid.draw(forest_plot)
 device <- dev.off()
 
